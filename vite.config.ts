e820fd3f5df68e52d877c89cbee6ed0dff6b7308
dsbure <69134282+dsbure@users.noreply.github.com>
2025-02-15 08:46:25 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/recard/' : '/',
+  base: '/',
   plugins: [
     react(),
     legacy()
