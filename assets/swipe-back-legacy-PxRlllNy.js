8c03ac036cf7fb485d5997cd7d15341e79c20164
dsbure <69134282+dsbure@users.noreply.github.com>
2025-02-17 21:14:45 +0800

    Updates

--- assets/swipe-back-legacy-PxRlllNy.js
+++ assets/swipe-back-legacy-PxRlllNy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CmBmgN-k.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Co3w6Wi_.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
