81950d295e5578557feae8bd04c594b41703ef76
dsbure <69134282+dsbure@users.noreply.github.com>
2025-02-28 12:20:04 +0800

    Updates

--- assets/swipe-back-legacy-CMeYWFWr.js
+++ assets/swipe-back-legacy-CMeYWFWr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-RxenjhOn.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CE6Xud4d.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
