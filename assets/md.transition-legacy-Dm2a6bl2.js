185ca53fc8ca566f9df8fb4f9a317af808e3f69d
dsbure <69134282+dsbure@users.noreply.github.com>
2025-02-28 17:41:15 +0800

    Updates

--- assets/md.transition-legacy-Dm2a6bl2.js
+++ assets/md.transition-legacy-Dm2a6bl2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CE6Xud4d.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BSWjvlHi.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
