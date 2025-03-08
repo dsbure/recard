b6a1735074c9d14173eaa9862fe2578b8f12335d
dsbure <69134282+dsbure@users.noreply.github.com>
2025-03-08 20:08:28 +0800

    dasdsadsadsad

--- src/pages/MainTab.tsx
+++ src/pages/MainTab.tsx
@@ -28,7 +28,7 @@ const MainTab: React.FC = () => {
   
   let pageViewLoaded = false;
   useEffect(() => {
-    FetchFlashcardData.getFlashcardData(false, import.meta.env.VITE_IN_DEVELOPMENT) //import.meta.env.VITE_IN_DEVELOPMENT
+    FetchFlashcardData.getFlashcardData(false, false) //import.meta.env.VITE_IN_DEVELOPMENT
       // really complicated for no reason whatsoever
       .then((data: IFlashcardData) => {
         if (!data?.categories) return;
