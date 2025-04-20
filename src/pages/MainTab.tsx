915ab8488e3f421a126998fd2f3bd8d9c4c7f2a0
dsbure <69134282+dsbure@users.noreply.github.com>
2025-04-20 14:06:35 +0800

    hfd

--- src/pages/MainTab.tsx
+++ src/pages/MainTab.tsx
@@ -1,4 +1,4 @@
-import { IonAvatar, IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonChip, IonContent, IonHeader, IonIcon, IonImg, IonItem, IonLabel, IonList, IonPage, IonPopover, IonSegment, IonSegmentButton, IonSegmentContent, IonSegmentView, IonSpinner, IonTitle, IonToggle, IonToolbar, ToggleCustomEvent, useIonAlert, useIonRouter, useIonViewWillEnter } from '@ionic/react';
+import { IonAvatar, IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonChip, IonContent, IonHeader, IonIcon, IonImg, IonItem, IonLabel, IonList, IonPage, IonPopover, IonSegment, IonSegmentButton, IonSegmentContent, IonSegmentView, IonSpinner, IonTitle, IonToggle, IonToolbar, ToggleCustomEvent, useIonAlert, useIonModal, useIonRouter, useIonViewWillEnter } from '@ionic/react';
 import './MainTab.css';
 import { arrowBack, bug, flash, heart, help, helpCircle, home, lockClosed, person, settings, trash } from 'ionicons/icons';
 import { TopicView } from '../components/TopicView';
@@ -13,6 +13,7 @@ import { TopicHeader } from '../components/TopicHeader';
 import { useThemeDetector } from '../hooks/useThemeDetector';
 import { usePopper } from '../hooks/Popper';
 import { IPlayerData } from '../interfaces/IPlayerData';
+import Markdown from 'react-markdown';
 
 const DebugButton: React.FC = () => {
   const [presentAlert] = useIonAlert();
@@ -50,12 +51,150 @@ const DebugButton: React.FC = () => {
   </IonButton>
 }
 const HelpButton: React.FC = () => {
-  const [presentAlert] = useIonAlert();
+  const [presentModal, dismissModal] = useIonModal(helpMarkdown, {
+    dismiss: (data: string) => dismissModal(data),
+  });
 
   return <IonButton
-    onClick={() => { }} shape="round">
+    onClick={() => { 
+      presentModal({
+        id: "help-modal"
+      });
+    }} shape="round">
     <IonIcon slot="icon-only" icon={helpCircle}></IonIcon>
   </IonButton>
+}
+
+// lol
+const helpMd = `# RECALL | Official Game Manual
+
+## THE LEGEND OF TROPICA
+
+Long ago, the island world of Tropica thrived in peace, protected by the Four Sacred Stones—powerful relics that upheld nature and knowledge.
+
+But when the shadowy creature Bombombini Gozini rose from the Seas of Turquoise, it began draining the world of its color and erasing memories.
+
+Now, as the chosen grandchild of the Island Chief, you are entrusted with the ancient Nautilus. Your mission: recover the Four Stones, forge the Nether Star, and bring knowledge and life back to Tropica.
+
+_**Will you recall what was forgotten?**_
+___
+
+## GAME OVERVIEW
+
+Recall is a quiz-based educational game created for Grade 7 students to strengthen their understanding of Integrated Science through story-driven challenges and interactive learning.
+___
+
+## OBJECTIVE
+
+- Play as the grandchild of the Island Chief (choose boy or girl)
+- Complete each island's science-based review challenges
+- Gather the Four Sacred Stones by finishing all review challenges each islad
+- Forge the Nether Star using the stones and the Nautilus
+- Defeat Bombombini Gozini and restore knowledge to Tropica
+___
+
+## ISLANDS & STONES
+
+Each island holds a specific scientific focus:
+
+- DILAWIKA – Force
+- LUNTIYA – Earthquakes and Ecosystems
+- BUGNAYA – Motion
+- ALABINA – Heat and Energy
+
+**_Note_**_: To unlock the next, you must complete all the challenges on one island._
+___
+
+## LEVELS & PROGRESSION
+
+**LEVELS**
+
+1. The game has 15 levels, starting from **Level 1: Nooblet** to **Level 15: Brainnotrot Ascended**. These levels are not tied to island difficulty; instead, they represent the player’s overall progress, similar to an experience bar.
+2. Players earn levels by accumulating **Aura Points**, which reflect effort and consistency.
+    1. Aura Points are earned based on:
+
+**_Speed_** – How quickly you complete the quiz
+
+**_Accuracy_** – How many answers do you get right
+
+**PROGRESSION**
+
+1. Each island contains **review challenges** (indicated by a star icon). To achieve mastery, a player must complete each review challenge **three times**. Once all challenges on an island are mastered, the player may proceed to the next island.
+2. Complete all four islands to forge the **Nether Star** and defeat the mythical villain, **Bombombini Gozini**.
+
+**_Note:_** _There is_ **_no fighting_** _in the game. Progress is made through_ **_story scenes_** _and_ **_quiz buttons_**_._
+___
+
+## QUIZ SYSTEM
+
+Each lesson includes:
+
+1. 10 questions
+2. 3 Hearts (lives)
+
+- 1 wrong answer = 1 lost heart
+- 3 wrong = failed attempt (below 75%)
+___
+
+## SIGMA MODES
+
+Get 4 correct answers in a row to activate Sigma Mode. Choose one power-up:
+
+1\. Skip Question – Instantly move to the next question
+
+2\. Time Freeze – Pause the timer temporarily
+
+3\. 50/50 – Remove half of the incorrect choices (Only for multiple choice)
+
+4\. Immunity – Ignore one incorrect answer without penalty
+
+**_Note:_**
+
+1. _If you obtained_ **_Sigma Mode_** _but didn’t activate it, your chosen power-up will still be available._
+2. _If you activated_ **_Immunity_** _and answered the question correctly, it will remain active._
+___
+
+## CONTROLS & INTERFACE
+
+**Tap** to answer questions and navigate dialogue
+
+**Scroll** to access lesson buttons listed vertically
+
+The game automatically saves after every level
+
+Designed with a clean and accessible interface for students.
+___
+
+## KEY FEATURES
+
+1. Quiz-based format built on the Grade 7 Science curriculum
+2. Engaging story-driven learning
+3. Lesson repetition to strengthen retention
+4. Interactive power-ups for strategic support
+5. Progress-tracking levels that motivate and reward effort
+___
+
+## TIPS FOR PLAYERS
+
+1. Pay attention to story moments!
+2. Use Sigma Mode power-ups wisely
+3. Each island’s content is tied to its science topic
+4. The more you remember, the further you’ll go
+
+**_ARE YOU READY TO RECALL?_**
+
+Collect the stones. Forge the star. Save Tropica.
+Let learning light the way.`;
+
+function helpMarkdown({ dismiss }: { dismiss: (data?: string) => void }) {
+  const ref = useRef<HTMLIonInputElement>(null);
+  return (
+    <IonContent id="help-modal-inner" class="ion-padding">
+      
+      <Markdown>{helpMd}</Markdown>
+      <IonButton onClick={() => dismiss()} id="help-close">Close</IonButton>
+    </IonContent>
+  );
 }
 
 const MainTab: React.FC = () => {
